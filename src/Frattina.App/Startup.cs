db25b614dee671faa8ef153b7438359b7c4ac3e8
Adriano Gavioli <52986801+adrianogavioli@users.noreply.github.com>
2020-08-29 00:16:29 -0300

    Add files via upload

---
+++ src/Frattina.App/Startup.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using Frattina.App.Configurations;
+using Frattina.Data.Context;
+using KissLog.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Frattina.App
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            //if (hostEnvironment.IsProduction())
+            //{
+            //    builder.AddUserSecrets<Startup>();
+            //}
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityConfiguration(Configuration);
+
+            services.AddAppSettingsConfiguration(Configuration);
+
+            services.AddDbContext<FrattinaDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddLogConfiguration();
+
+            services.AddMvcConfiguration(Configuration);
+
+            services.ResolveDependencies();
+
+            services.AddHttpClientConfiguration(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
+                app.UseHsts();
+            }
+
+            app.UseKissLogMiddleware();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseGlobalizationConfig();
+
+            app.UseLog(Configuration);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
